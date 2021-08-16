8d790238089e76c3623d7f622879d94463ed3d49
Kojiro0706 <s94.ogi.kojiro@gmail.com>
2021-08-16 23:42:26 +0900

    teamRepositoryクラスの作成

--- src/main/java/com/example/domain/Team.java
+++ src/main/java/com/example/domain/Team.java
@@ -1,17 +1,23 @@
 package com.example.domain;
 
+/**
+ * プロ野球チームを表すクラス.
+ * 
+ * @author kojiro0706
+ */
 public class Team {
 
+	/**　ID */
 	private Integer id;
-
+	/**　リーグ名 */
 	private String leagueName;
-
+	/**　球団名 */
 	private String teamName;
-
+	/**　本拠地 */
 	private String headquarters;
-
+	/**　発足 */
 	private String inauguration;
-
+	/**　歴史 */
 	private String history;
 
 	public Team() {
--- src/main/java/com/example/repository/TeamRepository.java
+++ src/main/java/com/example/repository/TeamRepository.java
@@ -1,5 +1,57 @@
 package com.example.repository;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Team;
+
+@Repository
 public class TeamRepository {
 
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	private static final RowMapper<Team> TEAM_ROW_MAPPER = new BeanPropertyRowMapper<>(Team.class);
+
+	
+	/**
+	 * 全件検索を行う.
+	 * 
+	 * @return プロ野球チーム情報
+	 */
+	public List<Team> findAll() {
+		String sql = "SELECT id,league_name, team_name, headquarters, inauguration, history"
+				+ "FROM teams ORDER BY inauguration;";
+
+		List<Team> teamList = template.query(sql, TEAM_ROW_MAPPER);
+
+		return teamList;
+	}
+	
+	/**
+	 * 主キー検索を行う.
+	 * 
+	 * @param id
+	 * @return プロ野球チーム情報
+	 */
+	public Team load(Integer id) {
+		String sql = "SELECT id,league_name, team_name, headquarters, inauguration, history"
+				+ "FROM teams WHERE id = :id";
+		
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		
+		Team team = template.queryForObject(sql, param, TEAM_ROW_MAPPER);
+		
+		return team;
+		
+	}
+
+	
 }
